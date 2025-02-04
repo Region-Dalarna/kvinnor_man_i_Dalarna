936fb15c84a593d4f197f2f7af7e0260d2e0a612
Frank Jon /Regional utvecklingsförvaltning /Falun <se2321000180-5gl1@ltdalarna.se>
2025-02-04 14:36:41 +0100

    Nytt diagram föräldrapenning per månad

--- Skript/1_korfil.R
+++ Skript/1_korfil.R
@@ -1,10 +1,22 @@
-output_mapp_figur = "Diagram/"
+if (!require("pacman")) install.packages("pacman")
+p_load(here)
+
+output_mapp_figur = here("Diagram","/")
 
 source("https://raw.githubusercontent.com/Region-Dalarna/diagram/refs/heads/main/diagram_matchning_lan_bakgrund.R")
 gg_matchning <- diag_matchning_lan(region_vekt = "20",
-                                   spara_figur = FALSE,
+                                   output_mapp_figur = output_mapp_figur,
+                                   spara_figur = TRUE,
                                    returnera_data = TRUE,
                                    kon_klartext = "*")
+
+source(here("Skript","fp_vab_manad.R"))
+gg_forsakringskassan <- diag_foraldrapenning_manad(region_vekt = "20",
+                                                   output_mapp = output_mapp_figur,
+                                                   diag_foraldrapenning = TRUE,
+                                                   diag_vab = FALSE,
+                                                   spara_diagrambildfil = TRUE,
+                                                   spara_dataframe_till_global_environment = TRUE)
 
 
 rmarkdown::render(
--- Skript/fp_vab_manad.R
+++ Skript/fp_vab_manad.R
@@ -3,9 +3,10 @@
 # Källa: https://www.dataportal.se/datasets/547_12671
 # Källa: https://www.dataportal.se/datasets/547_12364
 # =================================================================================================================
-test <- diag_foraldrapenning_manad(spara_diagrambildfil = TRUE)
 diag_foraldrapenning_manad<-function(region_vekt = "20", 
-                                     output_mapp = here("Diagram","/"),
+                                     diag_foraldrapenning = TRUE,
+                                     diag_vab = TRUE,
+                                     output_mapp = "G:/Samhällsanalys/Statistik/Näringsliv/basfakta/",
                                      spara_diagrambildfil = FALSE,
                                      spara_dataframe_till_global_environment = FALSE){
   
@@ -17,7 +18,6 @@ diag_foraldrapenning_manad<-function(region_vekt = "20",
          here)
   
   # Funktioner som behövs
-  #source("https://raw.githubusercontent.com/JonFrank81/funktioner_alternativ/main/hamta_data.R")
   source("https://raw.githubusercontent.com/Region-Dalarna/funktioner/main/func_SkapaDiagram.R")
   source("https://raw.githubusercontent.com/Region-Dalarna/funktioner/main/func_API.R")
   
@@ -28,72 +28,74 @@ diag_foraldrapenning_manad<-function(region_vekt = "20",
   flik_lista = list()
   
   gg_list = list()
-
-  #json_test = hamta_fk_json_dataset_med_url("https://www.forsakringskassan.se/fk_apps/MEKAREST/public/v1/fp-antal-mottagare-nettodagar-belopp/FPAntalDagarBeloppLanKommun.json")
   
-  foraldrapenning_df = hamta_excel_dataset_med_url(path[1],skippa_rader = 2) %>% 
-    filter(substr(Län,1,2) %in% region_vekt) %>%
-     filter(Kommun == "20 Dalarnas län") %>% 
-      rename(Antal_mottagare = `Antal mottagare`,
-             Andel_kon = `Andel nettodagar per kön`) %>%  
-        select(-kolumnnamn) %>% 
-          mutate(Månad = ifelse(substr(Månad,1,1)==0,substr(Månad,2,2),Månad)) 
-          
-    # foraldrapenning_df = foraldrapenning_df %>% manader_bearbeta_scbtabeller(kolumn_manad = "Månad_test")
+  if(diag_foraldrapenning == TRUE){
+
+    foraldrapenning_df = hamta_excel_dataset_med_url(path[1],skippa_rader = 2) %>% 
+      filter(substr(Län,1,2) %in% region_vekt) %>%
+       filter(Kommun == "20 Dalarnas län") %>% 
+        rename(Antal_mottagare = `Antal mottagare`,
+               Andel_kon = `Andel nettodagar per kön`) %>%  
+          select(-kolumnnamn) %>% 
+            mutate(Månad = ifelse(substr(Månad,1,1)==0,substr(Månad,2,2),Månad)) 
       
-      foraldrapenning_df$Månad = case_when(
-                            foraldrapenning_df$Månad == 1 ~ "Januari",
-                            foraldrapenning_df$Månad == 2 ~ "Februari",
-                            foraldrapenning_df$Månad == 3 ~ "Mars",
-                            foraldrapenning_df$Månad == 4 ~ "April",
-                            foraldrapenning_df$Månad == 5 ~ "Maj",
-                            foraldrapenning_df$Månad == 6 ~ "Juni",
-                            foraldrapenning_df$Månad == 7 ~ "Juli",
-                            foraldrapenning_df$Månad == 8 ~ "Augusti",
-                            foraldrapenning_df$Månad == 9 ~ "September",
-                            foraldrapenning_df$Månad == 10 ~ "Oktober",
-                            foraldrapenning_df$Månad == 11 ~ "November",
-                            foraldrapenning_df$Månad == 12 ~ "December"
-                          )
-      
-      if(spara_dataframe_till_global_environment) {
-        assign("foraldrapenning_manad_df", foraldrapenning_df, envir = .GlobalEnv)
-      }
-      
-      # Create factor-variable for month
-      foraldrapenning_df$Månad = factor(foraldrapenning_df$Månad, levels = c("Januari", "Februari", "Mars", "April", "Maj", "Juni", "Juli", "Augusti", "September", "Oktober", "November", "December"))
-      
-      diagram_capt <- "Källa: Försäkringskassan.\nBearbetning: Samhällsanalys, Region Dalarna."
-      diagramtitel <- "Uttagna nettodagar av föräldrapenning i Dalarna"
-      diagramfilnamn <- "foraldrapenning_manad.png"
-      
-      
-      gg_obj <- SkapaStapelDiagram(skickad_df = foraldrapenning_df%>%
-                                           filter(Kommun == "20 Dalarnas län",
-                                                  Kön != "Kvinnor och män",
-                                                  År>"2018") %>% 
-                                           mutate("år" = År), 
-                                         skickad_x_var = "Månad", 
-                                         skickad_y_var = "Nettodagar", 
-                                         skickad_x_grupp = "år",
-                                         diagram_facet = TRUE,
-                                         facet_scale = "fixed",
-                                         facet_grp = "Kön",
-                                         manual_color = diagramfarger("rus_sex"),
-                                         manual_x_axis_text_vjust = 1,
-                                         manual_x_axis_text_hjust = 1,
-                                         facet_legend_bottom = TRUE,
-                                         #berakna_index = TRUE,
-                                         x_axis_lutning = 45,
-                                         diagram_titel = diagramtitel,
-                                         diagram_capt =  diagram_capt,
-                                         stodlinjer_avrunda_fem = TRUE,
-                                         output_mapp = output_mapp,
-                                         filnamn_diagram = diagramfilnamn,
-                                         skriv_till_diagramfil = spara_diagrambildfil)
-      
-  gg_list <- c(gg_list, list(gg_obj))
-  names(gg_list)[[length(gg_list)]] <- diagramfilnamn %>% str_remove(".png")
+        foraldrapenning_df$Månad = case_when(
+                              foraldrapenning_df$Månad == 1 ~ "Januari",
+                              foraldrapenning_df$Månad == 2 ~ "Februari",
+                              foraldrapenning_df$Månad == 3 ~ "Mars",
+                              foraldrapenning_df$Månad == 4 ~ "April",
+                              foraldrapenning_df$Månad == 5 ~ "Maj",
+                              foraldrapenning_df$Månad == 6 ~ "Juni",
+                              foraldrapenning_df$Månad == 7 ~ "Juli",
+                              foraldrapenning_df$Månad == 8 ~ "Augusti",
+                              foraldrapenning_df$Månad == 9 ~ "September",
+                              foraldrapenning_df$Månad == 10 ~ "Oktober",
+                              foraldrapenning_df$Månad == 11 ~ "November",
+                              foraldrapenning_df$Månad == 12 ~ "December"
+                            )
+        
+        if(spara_dataframe_till_global_environment) {
+          assign("foraldrapenning_manad_df", foraldrapenning_df, envir = .GlobalEnv)
+        }
+        
+        # Create factor-variable for month
+        foraldrapenning_df$Månad = factor(foraldrapenning_df$Månad, levels = c("Januari", "Februari", "Mars", "April", "Maj", "Juni", "Juli", "Augusti", "September", "Oktober", "November", "December"))
+        
+        diagram_capt <- "Källa: Försäkringskassan.\nBearbetning: Samhällsanalys, Region Dalarna."
+        diagramtitel <- "Uttagna nettodagar av föräldrapenning i Dalarna"
+        diagramfilnamn <- "foraldrapenning_manad.png"
+        
+        
+        gg_obj <- SkapaStapelDiagram(skickad_df = foraldrapenning_df%>%
+                                             filter(Kommun == "20 Dalarnas län",
+                                                    Kön != "Kvinnor och män",
+                                                    År>"2018") %>% 
+                                             mutate("år" = År), 
+                                           skickad_x_var = "Månad", 
+                                           skickad_y_var = "Nettodagar", 
+                                           skickad_x_grupp = "år",
+                                           diagram_facet = TRUE,
+                                           facet_scale = "fixed",
+                                           facet_grp = "Kön",
+                                           manual_color = diagramfarger("rus_sex"),
+                                           manual_x_axis_text_vjust = 1,
+                                           manual_x_axis_text_hjust = 1,
+                                           facet_legend_bottom = TRUE,
+                                           #berakna_index = TRUE,
+                                           x_axis_lutning = 45,
+                                           diagram_titel = diagramtitel,
+                                           diagram_capt =  diagram_capt,
+                                           stodlinjer_avrunda_fem = TRUE,
+                                           output_mapp = output_mapp,
+                                           filnamn_diagram = diagramfilnamn,
+                                           skriv_till_diagramfil = spara_diagrambildfil)
+        
+    gg_list <- c(gg_list, list(gg_obj))
+    names(gg_list)[[length(gg_list)]] <- diagramfilnamn %>% str_remove(".png")
+  
+  }
+  
+  if(diag_vab == TRUE){
       
   vab_df <- hamta_excel_dataset_med_url(path[2],skippa_rader = 2) %>% 
     filter(substr(Län,1,2) %in% region_vekt) %>%
@@ -157,6 +159,7 @@ diag_foraldrapenning_manad<-function(region_vekt = "20",
   gg_list <- c(gg_list, list(gg_obj))
   diagramfil <- "tf_foraldrapenning_manad.png"
   names(gg_list)[[length(gg_list)]] <- diagramfilnamn %>% str_remove(".png")
+  }
   
   return(gg_list)
   
