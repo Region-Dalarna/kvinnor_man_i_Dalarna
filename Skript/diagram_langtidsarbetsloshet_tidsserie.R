67f3bfae1c7b523cd7f905994068a58920700f25
Frank Jon /Regional utvecklingsförvaltning /Falun <se2321000180-5gl1@ltdalarna.se>
2025-07-15 09:22:46 +0200

    Ändrat mindre felaktighet

--- Skript/diagram_langtidsarbetsloshet_tidsserie.R
+++ Skript/diagram_langtidsarbetsloshet_tidsserie.R
@@ -28,7 +28,7 @@ diag_langtidsarbetsloshet<-function(region_vekt = "20",
     assign("långtidsarbetslöshet", långtidsarbetslöshet, envir = .GlobalEnv)
   }
   
-  diagram_capt <- "Källa: Arbetsförmedlingen (via Kolada)\nBearbetning: Samhällsanalys, Region Dalarna\nDiagramförklaring: Antal invånare 25-64 år (årsmedelvärde år T) som varit öppet arbetslösa eller i program med aktivitetsstöd i minst sex månader,\ndividerat med antal invånare 25-64 år den 31/12 år T-1."
+  diagram_capt <- "Källa: Arbetsförmedlingen (via Kolada)\nBearbetning: Samhällsanalys, Region Dalarna\nDiagramförklaring: Antal invånare 25-64 år (årsmedelvärde år T) som varit öppet arbetslösa eller i program\nmed aktivitetsstöd i minst sex månader,dividerat med antal invånare 25-64 år den 31/12 år T-1."
   diagramtitel <- paste0("Långtidsarbetslöshet 25-64 år i ",unique(långtidsarbetslöshet$region))
   diagramfilnamn <- paste0("langtidsarbetsloshet_kolada_",unique(långtidsarbetslöshet$region),".png")
   
