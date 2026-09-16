acc575491bc4013979d76937da658d48df2764fb
Frank Jon /Regional utvecklingsförvaltning /Falun <se2321000180-5gl1@ltdalarna.se>
2026-09-16 08:23:16 +0200

    Ändrat så att caption blir korrekt

--- Skript/diagram_arbetsloshet_AF_bakgrund_ny.R
+++ Skript/diagram_arbetsloshet_AF_bakgrund_ny.R
@@ -21,10 +21,6 @@ diag_arbetsloshet_2008_senastear <- function(output_mapp = "G:/skript/jon/Slask/
   
   gg_list <- list()
   
-  # Läser in data från Excel
-  
- 
-  
   # ========================================== Läser in data ============================================
   # URL från arbetsförmedlingen
   af_url <- "https://arbetsformedlingen.se/statistik/sok-statistik/tidigare-statistik-tidsserier"
@@ -49,25 +45,58 @@ diag_arbetsloshet_2008_senastear <- function(output_mapp = "G:/skript/jon/Slask/
     paste0("https://arbetsformedlingen.se", .)
   
   # Använder funktioner för att hämta data från de två källorna (en gammal och en ny)
-  url_old = inlasfil[2]  # the one going up to 2023-12 (reg)
+  url_old = inlasfil[2]  # the one going up to 2022-12 (reg)
   url_new <- inlasfil[1]  # the one starting 2023-01 (bas)
   
   old_df <- process_url(url_old)
   new_df <- process_url(url_new)
   
-  # Slår ihop de två datakällorna och bearbetar data för diagrammet. Sedan tas de bort från miljön
-  andel_df <- combine_old_new(old_df, new_df, cutoff = "2022-12") %>% 
+  cutoff_datum <- "2022-12"
+  cutoff_ar <- str_sub(cutoff_datum, 1, 4)
+  
+  # Slår ihop de två datakällorna och bearbetar data för diagrammet.
+  regioner_df <- combine_old_new(old_df, new_df, cutoff = cutoff_datum) %>% 
     filter(KOM == "Dalarnas län") %>% 
-      separate(PERIOD,c("Ar","Manad"),"-") %>%
-        rename(Region = KOM) %>% 
-          select(c(Ar,Manad,Region,`Inrikes KVI`,`Inrikes MÄN`,`Utrikes KVI`,`Utrikes MÄN`)) %>% 
-            pivot_longer(4:7,names_to = "Grupp",values_to = "Arbetslöshet") %>%
-              separate(Grupp, c("Grupp", "Kon"), sep = " ", extra = "merge") %>% 
-                mutate(Kon = ifelse(Kon == "KVI", "Kvinnor","Män")) %>%
-                  group_by(Ar,Region,Grupp,Kon) %>% 
-                    summarize(Arbetslöshet = mean(Arbetslöshet)*100) %>% 
-                      mutate(Grupp = ifelse(Grupp == "Inrikes","Inrikes födda","Utrikes födda")) %>% 
-                        ungroup()
+    separate(PERIOD,c("Ar","Manad"),"-") %>%
+    rename(Region = KOM)
+  
+  # Tar reda på vilka månader som faktiskt finns med för det senaste (ofta ofullständiga)
+  # året, så att diagramtexten uppdateras automatiskt utan att behöva ändras för hand.
+  svenska_manadsnamn <- c("januari", "februari", "mars", "april", "maj", "juni",
+                          "juli", "augusti", "september", "oktober", "november", "december")
+  
+  senaste_ar <- max(regioner_df$Ar)
+  senaste_manad_nr <- regioner_df %>%
+    filter(Ar == senaste_ar) %>%
+    dplyr::pull(Manad) %>%
+    as.numeric() %>%
+    max()
+  senaste_manad_namn <- svenska_manadsnamn[senaste_manad_nr]
+  
+  # Plockar ut åldersintervallen (t.ex. "16-64 år"/"16-65 år") ur rubrikraderna som
+  # lästes in i process_url(), istället för att återge hela rubriktexten ordagrant.
+  alla_rubriker <- c(attr(old_df, "rubrik"), attr(new_df, "rubrik"))
+  alder_forekomster <- str_extract(alla_rubriker, "\\d{2}-\\d{2} år") %>%
+    unique() %>%
+    sort()
+  alder_forekomster <- alder_forekomster[!is.na(alder_forekomster)]
+  
+  alder_capt <- if (length(alder_forekomster) >= 2) {
+    glue("Inskrivna arbetslösa i procent av arbetskraften. Till och med {cutoff_ar}, {alder_forekomster[1]}, därefter {alder_forekomster[2]}.")
+  } else if (length(alder_forekomster) == 1) {
+    glue("Inskrivna arbetslösa i procent av arbetskraften ({alder_forekomster[1]}).")
+  } else {
+    "Inskrivna arbetslösa i procent av arbetskraften."
+  }
+  
+  andel_df <- regioner_df %>% 
+    select(c(Ar,Manad,Region,`Inrikes KVI`,`Inrikes MÄN`,`Utrikes KVI`,`Utrikes MÄN`)) %>% 
+    pivot_longer(4:7,names_to = "Grupp",values_to = "Arbetslöshet") %>%
+    separate(Grupp, c("Grupp", "Kon"), sep = " ", extra = "merge") %>% 
+    mutate(Kon = ifelse(Kon == "KVI", "Kvinnor","Män")) %>%
+    group_by(Ar,Region,Grupp,Kon) %>% 
+    summarize(Arbetslöshet = mean(Arbetslöshet)*100, .groups = "drop") %>% 
+    mutate(Grupp = ifelse(Grupp == "Inrikes","Inrikes födda","Utrikes födda"))
   
   rm(old_df,new_df)
   
@@ -75,10 +104,15 @@ diag_arbetsloshet_2008_senastear <- function(output_mapp = "G:/skript/jon/Slask/
     assign("arbetslosa_af_tidsserie_df", andel_df, envir = .GlobalEnv)
   }
   
+  diagram_capt <- paste(c(
+    "Källa: Arbetsförmedlingen.",
+    "Bearbetning: Samhällsanalys, Region Dalarna.",
+    "Diagramförklaring: Månadsdata. Diagrammet visar årsmedelvärden.",
+    alder_capt,
+    glue("Data för {senaste_ar} till och med {senaste_manad_namn}")
+  ), collapse = "\n")
   
-  diagram_capt <- glue("Källa: Arbetsförmedlingen.\nBearbetning: Samhällsanalys, Region Dalarna.\nDiagramförklaring: Månadsdata. Diagrammet visar årsmedelvärden; för {max(andel_df$Ar)} visas medelvärdet för hittills\ntillgängliga månader") 
-  
-  diagramtitel <- paste0("Arbetslöshet 16-64 år i ",unique(andel_df$Region))
+  diagramtitel <- paste0("Arbetslöshet i ",unique(andel_df$Region))
   diagramfilnamn <- paste0("arbetsloshet_tidsserie_bakgrund_",unique(andel_df$Region),".png")
   
   gg_obj <- SkapaStapelDiagram(skickad_df = andel_df, 
